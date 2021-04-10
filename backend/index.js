d056ea7015600f55ac0fb04bd366bee519afc086
abrmr <iwcw4i@gmail.com>
2021-04-11 00:14:05 +0200

    igonre idea files, set prettier

--- backend/index.js
+++ backend/index.js
@@ -2,26 +2,22 @@ const express = require('express');
 const students = require('./students');
 const resources = require('./resources');
 
-const {
-  PORT = 3000,
-} = process.env
+const { PORT = 4000 } = process.env;
 
 const run = async () => {
-
-  const app = express()
-  app.use(express.json())
+  const app = express();
+  app.use(express.json());
   app.use('/public', express.static('resources'));
 
   /* Application */
   const router = express.Router();
   router.get('/resources', (req, res) => {
-    res.json({resources});
+    res.json({ resources });
   });
 
   router.get('/students', (req, res) => {
-    res.json({students})
+    res.json({ students });
   });
-
 
   /**
    * POST body to save an assignment
@@ -36,23 +32,27 @@ const run = async () => {
   router.post('/assignment', (req, res) => {
     const messages = [];
 
-    if (!req.body.name) messages.push("Please add an assignment name")
-    if (!req.body.date) messages.push("Please add an assignment due date")
-    if (!req.body.resources) messages.push("Please add a least 1 resource for the assignment")
-    if (!req.body.students) messages.push("Please provide a list of students assigned to the task")
+    if (!req.body.name) messages.push('Please add an assignment name');
+    if (!req.body.date) messages.push('Please add an assignment due date');
+    if (!req.body.resources)
+      messages.push('Please add a least 1 resource for the assignment');
+    if (!req.body.students)
+      messages.push('Please provide a list of students assigned to the task');
 
-    if (messages.length > 0) return res.status(400).json({messages});
-    return res.status(201).json({message: "Accepted assignment response successfully"});
-  })
+    if (messages.length > 0) return res.status(400).json({ messages });
+    return res
+      .status(201)
+      .json({ message: 'Accepted assignment response successfully' });
+  });
 
   app.use('/', router);
 
   const server = app.listen(PORT, () => {
-    const { address, port } = server.address()
-    console.log(`Listening: http://${address}:${port}`)
-  })
-}
+    const { address, port } = server.address();
+    console.log(`Listening: http://${address}:${port}`);
+  });
+};
 
 run().then(() => {
-  console.log('Started server')
+  console.log('Started server');
 });
